dcdd5f8c84e75c96220b4d711ecbdf7c81754a52
cocotran <anhquangtran2703@gmail.com>
2021-04-19 00:26:12 -0400

    Added sky background, lap time tracker

--- Racing.cpp
+++ Racing.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <string>
 
 using namespace std;
 
@@ -18,8 +19,14 @@ private:
 	float fSpeed = 0.0f;
 
 	float fCurvature = 0.0f;
+	float fTrackCurvature = 0.0f;
+	float fPlayerCurvature = 0.0f;
+	float fTrackDistance = 0.0f;
+
+	float fCurrentLapTime = 0.0f;
 
 	vector<pair<float, float>> vecTrack;	// curvature, distance
+	list<float> listLapTimes;
 
 protected:
 
@@ -34,6 +41,14 @@ protected:
 		vecTrack.push_back(make_pair(-1.0f, 200.0f));
 		vecTrack.push_back(make_pair(1.0f, 200.0f));
 
+
+		for (auto t : vecTrack)
+			fTrackDistance += t.second;
+
+
+		listLapTimes = { 0,0,0,0,0 };
+		fCurrentLapTime = 0.0f;
+
 		return true;
 	}
 
@@ -45,6 +60,26 @@ protected:
 		else
 			fSpeed -= 1.0f * fElapsedTime;
 
+		int nCarDirection = 0;
+
+		// Steering 
+		if (m_keys[VK_LEFT].bHeld)
+		{
+			fPlayerCurvature -= 0.7f * fElapsedTime;
+			nCarDirection = -1;
+		}
+			
+		if (m_keys[VK_RIGHT].bHeld)
+		{
+			fPlayerCurvature += 0.7f * fElapsedTime;
+			nCarDirection = +1;
+		}
+			
+
+		// Making the grass more "sticky"
+		if (fabs(fPlayerCurvature - fTrackCurvature) >= 0.8f)
+			fSpeed -= 5.0f * fElapsedTime;
+
 		// Clamp Speed
 		if (fSpeed < 0.0f) fSpeed = 0.0f;
 		if (fSpeed > 1.0f) fSpeed = 1.0f;
@@ -55,6 +90,16 @@ protected:
 		// Get Point on track
 		float fOffset = 0;
 		int nTrackSection = 0;
+
+		fCurrentLapTime += fElapsedTime;
+		if (fDistance >= fTrackDistance)
+		{
+			fDistance -= fTrackDistance;
+			listLapTimes.push_front(fCurrentLapTime);
+			listLapTimes.pop_back();
+			fCurrentLapTime = 0.0f;
+		}
+			
 
 		// Find position on track (could optimise)
 		while (nTrackSection < vecTrack.size() && fOffset <= fDistance)
@@ -68,8 +113,25 @@ protected:
 		float fTrackCurveDiff = (fTargetCurvature - fCurvature) * fElapsedTime * fSpeed;
 		fCurvature += fTrackCurveDiff;
 
+		fTrackCurvature += (fCurvature)*fElapsedTime * fSpeed;
 
-		Fill(0, 0, ScreenWidth(), ScreenHeight(), L' ', 0);	// Initialize the screen as an empty space.....
+
+		// Draw Sky - light blue and dark blue
+		for (int y = 0; y < ScreenHeight() / 2; y++)
+			for (int x = 0; x < ScreenWidth(); x++)
+				Draw(x, y, y < ScreenHeight() / 4 ? PIXEL_HALF : PIXEL_SOLID, FG_DARK_BLUE);
+
+
+		// Draw Scenery - our hills are a rectified sine wave
+		for (int x = 0; x < ScreenWidth(); x++)
+		{
+			int nHillHeight = (int)(fabs(sinf(x * 0.01f + fTrackCurvature) * 16.0f));
+			for (int y = (ScreenHeight() / 2) - nHillHeight; y < ScreenHeight() / 2; y++)
+				Draw(x, y, PIXEL_SOLID, FG_DARK_YELLOW);
+		}
+
+
+		//Fill(0, 0, ScreenWidth(), ScreenHeight(), L' ', 0);	// Initialize the screen as an empty space.....
 
 		// Drawing the track
 		// The track begins from the middle of the screen vertically 
@@ -97,12 +159,16 @@ protected:
 				int nGrassColour = sinf(20.0f * powf(1.0f - fPerspective, 3) + fDistance * 0.1f) > 0.0f ? FG_GREEN : FG_DARK_GREEN;
 				int nClipColour = sinf(80.0f * powf(1.0f - fPerspective, 3) + fDistance * 0.1f) > 0.0f ? FG_RED : FG_WHITE;
 
+
+				int nRoadColor = (nTrackSection - 1) == 0 ? FG_WHITE : FG_GREY;
+
+
 				if (x >= 0 && x < nLeftGrass)
 					Draw(x, nRow, PIXEL_SOLID, nGrassColour);
 				if (x >= nLeftGrass && x < nLeftClip)
 					Draw(x, nRow, PIXEL_SOLID, nClipColour);
 				if (x >= nLeftClip && x < nRightClip)
-					Draw(x, nRow, PIXEL_SOLID, FG_GREY);
+					Draw(x, nRow, PIXEL_SOLID, nRoadColor);
 				if (x >= nRightClip && x < nRightGrass)
 					Draw(x, nRow, PIXEL_SOLID, nClipColour);
 				if (x >= nRightGrass && x < ScreenWidth())
@@ -112,16 +178,70 @@ protected:
 
 
 		// Draw car
+		fCarPos = fPlayerCurvature - fTrackCurvature;
 		int nCarPos = ScreenWidth() / 2 + ((int)(ScreenWidth() * fCarPos) / 2.0f) - 7;
 
-		DrawStringAlpha(nCarPos, 80, L"   ||****||   ");
-		DrawStringAlpha(nCarPos, 81, L"   ||****||   ");
-		DrawStringAlpha(nCarPos, 82, L"      ##      ");
-		DrawStringAlpha(nCarPos, 83, L"     #$$#     ");
-		DrawStringAlpha(nCarPos, 84, L"     #$$#     ");
-		DrawStringAlpha(nCarPos, 85, L"|||  ####  |||");
-		DrawStringAlpha(nCarPos, 86, L"|||********|||");
-		DrawStringAlpha(nCarPos, 87, L"|||  ####  |||");
+		switch (nCarDirection)
+		{
+		case 0:
+			DrawStringAlpha(nCarPos, 80, L"   ||####||   ");
+			DrawStringAlpha(nCarPos, 81, L"      ##      ");
+			DrawStringAlpha(nCarPos, 82, L"     ####     ");
+			DrawStringAlpha(nCarPos, 83, L"     ####     ");
+			DrawStringAlpha(nCarPos, 84, L"|||  ####  |||");
+			DrawStringAlpha(nCarPos, 85, L"|||########|||");
+			DrawStringAlpha(nCarPos, 86, L"|||  ####  |||");
+			break;
+
+		case +1:
+			DrawStringAlpha(nCarPos, 80, L"      //####//");
+			DrawStringAlpha(nCarPos, 81, L"         ##   ");
+			DrawStringAlpha(nCarPos, 82, L"       ####   ");
+			DrawStringAlpha(nCarPos, 83, L"      ####    ");
+			DrawStringAlpha(nCarPos, 84, L"///  ####//// ");
+			DrawStringAlpha(nCarPos, 85, L"//#######///O ");
+			DrawStringAlpha(nCarPos, 86, L"/// #### //// ");
+			break;
+
+		case -1:
+			DrawStringAlpha(nCarPos, 80, L"\\\\####\\\\      ");
+			DrawStringAlpha(nCarPos, 81, L"   ##         ");
+			DrawStringAlpha(nCarPos, 82, L"   ####       ");
+			DrawStringAlpha(nCarPos, 83, L"    ####      ");
+			DrawStringAlpha(nCarPos, 84, L" \\\\\\\\####  \\\\\\");
+			DrawStringAlpha(nCarPos, 85, L" O\\\\\\#######\\\\");
+			DrawStringAlpha(nCarPos, 86, L" \\\\\\\\ #### \\\\\\");
+			break;
+		}
+
+
+		// Draw Stats
+		DrawString(0, 0, L"Distance: " + to_wstring(fDistance));
+		DrawString(0, 1, L"Target Curvature: " + to_wstring(fCurvature));
+		DrawString(0, 2, L"Player Curvature: " + to_wstring(fPlayerCurvature));
+		DrawString(0, 3, L"Player Speed: " + to_wstring(fSpeed));
+		DrawString(0, 4, L"Track Curvature: " + to_wstring(fTrackCurvature));
+
+
+
+		auto disp_time = [](float t)	// Lambda expression
+		{
+			int nMinutes = t / 60.0f;
+			int nSeconds = t - (nMinutes * 60.0f);
+			int nMilliSeconds = (t - (float)nSeconds) * 1000.0f;
+
+			return to_wstring(nMinutes) + L"." + to_wstring(nSeconds) + L":" + to_wstring(nMilliSeconds);
+		};
+
+		DrawString(10, 8, disp_time(fCurrentLapTime));
+
+		// Display last 5 lap times
+		int j = 10;
+		for (auto l : listLapTimes)
+		{
+			DrawString(10, j, disp_time(l));
+			j++;
+		}
 
 
 		return true;
